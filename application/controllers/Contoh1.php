e60602090d62e3d7d2ee03e3be12a4f159e5df73
sidikaljabar01 <muhammadaljabar58062@gmail.com>
2024-03-22 08:32:32 +0700

    First Commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya Muhamad Sidik Aljabar
+        Saya tinggal di daerah bojonggede hoby yang saya sukai adalah bermain game";
+    }
+}
